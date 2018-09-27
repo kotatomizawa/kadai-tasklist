f9ddfba9e9820097f755cae5d712e063997afe37
kota.t <tccavudi@gmail.com>
2018-09-27 02:43:41 +0000

    for resubmit post function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
   def index
     if logged_in?
       @task = current_user.tasks.build  
@@ -8,7 +10,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    set_task
+    @task = current_user.tasks.find_by(id: params[:id])
   end
   
   def new
@@ -21,19 +23,18 @@ class TasksController < ApplicationController
       flash[:success] = 'task added'
       redirect_to root_url
     else
-      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      @tasks = current_user
       flash.now[:danger] = 'task not added'
       render 'new'
     end
   end
   
   def edit
-   set_task 
+    @task = current_user.tasks.find_by(id: params[:id])
   end
   
   def update
-    set_task
-
+    @task = current_user.tasks.find_by(params[:id])
     if @task.update(task_params)
       flash[:success] = 'メッセージが正常に編集されました'
       redirect_to @task
@@ -44,23 +45,24 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    set_task
-    @micropost.destroy
-    flash[:success] = 'tasl deleted'
+    @task.destroy
+    flash[:success] = 'task deleted'
     redirect_back(fallback_location: root_path)
   end  
     
-    
 
+  private
+  
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end  
+  
+  def correct_user
+      @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_url
+      end
+  end
 
-private
-
-def set_task
-  @task = Task.find(params[:id])
-end
-
-def task_params
-  params.require(:task).permit(:content, :status)
 end  
-
-end
